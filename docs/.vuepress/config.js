20638b67470ff07cadf4d108819df5b8ffdd637f
foxy pirate cove / Fnaf <108185011+foxypiratecove37350@users.noreply.github.com>
2024-10-04 11:53:25 +0000

    VuePress setup

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,30 @@
+import { defaultTheme } from '@vuepress/theme-default'
+import { searchPlugin } from '@vuepress/plugin-search'
+import { defineUserConfig } from 'vuepress/cli'
+import { viteBundler } from '@vuepress/bundler-vite'
+
+export default defineUserConfig({
+	lang: 'en-US',
+
+	title: 'x96 Reference',
+	description: 'Reference of all x96 Assembly instructions and opcodes',
+
+	theme: defaultTheme({
+		logo: 'https://avatars.githubusercontent.com/u/183313352',
+		docsRepo: 'https://github.com/Ignitem/x96-reference',
+		repo: 'https://github.com/Ignitem/x96-reference',
+		navbar: ['/', '/get-started'],
+	}),
+
+	head: [
+		['link', { rel: 'icon', href: 'https://avatars.githubusercontent.com/u/183313352' }],
+	],
+
+	plugins: [
+		searchPlugin({
+			maxSuggestions: 10,
+		}),
+	],
+
+	bundler: viteBundler(),
+})
